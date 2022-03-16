d66da0b1511e8a7d130ae790c732bf6195acf049
jostein-tollefsrud <mail@josteintollefsrud.no>
2022-03-16 19:02:48 +0100

    add dotenv

--- src/redux/actions/plantActions.js
+++ src/redux/actions/plantActions.js
@@ -1,236 +1,232 @@
 import * as actionTypes from '../constants/plantConstants';
 import axios from 'axios';
+require('dotenv').config();
 
 //const userInfo = JSON.parse(localStorage.getItem('userInfo'));
 
 let token = undefined;
+
+const { PLANT_API } = process.env;
 
 /*if (userInfo) {
     token = userInfo.token;
 }*/
 
 export const getPlants = () => async (dispatch) => {
-    try {
-        dispatch({ type: actionTypes.GET_PLANT_REQUEST });
+  try {
+    dispatch({ type: actionTypes.GET_PLANT_REQUEST });
 
-        const { data } = await axios.get(
-            'https://ntnu-plant-manager.herokuapp.com/plants'
-        );
+    const { data } = await axios.get(`${PLANT_API}/plants`);
 
-        dispatch({
-            type: actionTypes.GET_PLANT_SUCCESS,
-            payload: data,
-        });
-    } catch (error) {
-        dispatch({
-            type: actionTypes.GET_PLANT_FAIL,
-            payload:
-                error.response && error.response.data.message
-                    ? error.response.data.message
-                    : error.message,
-        });
-    }
+    dispatch({
+      type: actionTypes.GET_PLANT_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: actionTypes.GET_PLANT_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
 };
 
 export const getPlantDetails = (id) => async (dispatch) => {
-    dispatch({ type: actionTypes.GET_PLANT_DETAILS_REQUEST });
-    try {
-        const { data } = await axios.get(
-            `https://ntnu-plant-manager.herokuapp.com/plants/${id}`
-        );
+  dispatch({ type: actionTypes.GET_PLANT_DETAILS_REQUEST });
+  try {
+    const { data } = await axios.get(`${PLANT_API}/plants/${id}`);
 
-        dispatch({
-            type: actionTypes.GET_PLANT_DETAILS_SUCCESS,
-            payload: data,
-        });
-    } catch (error) {
-        dispatch({
-            type: actionTypes.GET_PLANT_DETAILS_FAIL,
-            payload:
-                error.response && error.response.data.message
-                    ? error.response.data.message
-                    : error.message,
-        });
-    }
+    dispatch({
+      type: actionTypes.GET_PLANT_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: actionTypes.GET_PLANT_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
 };
 
 export const createPlant =
-    (name, location, waterFrequency, fertilizingFrequency, light) =>
-    async (dispatch) => {
-        dispatch({
-            type: actionTypes.PLANT_CREATE_REQUEST,
-            payload: {
-                name,
-                location,
-                waterFrequency,
-                fertilizingFrequency,
-                light,
-            },
-        });
-        try {
-            const userInfo = JSON.parse(localStorage.getItem('userInfo'));
-
-            const { data } = await axios.post(
-                'https://ntnu-plant-manager.herokuapp.com/register',
-                {
-                    name,
-                    location,
-                    waterFrequency,
-                    fertilizingFrequency,
-                    light,
-                },
-                {
-                    headers: { Authorization: `Bearer ${userInfo.token}` },
-                }
-            );
-            dispatch({
-                type: actionTypes.PLANT_CREATE_SUCCESS,
-                payload: data,
-            });
-        } catch (error) {
-            dispatch({
-                type: actionTypes.PLANT_CREATE_FAIL,
-                payload:
-                    error.response && error.response.data.message
-                        ? error.response.data.message
-                        : error.message,
-            });
-        }
-    };
-
-export const updatePlant =
-    (
+  (name, location, waterFrequency, fertilizingFrequency, light) =>
+  async (dispatch) => {
+    dispatch({
+      type: actionTypes.PLANT_CREATE_REQUEST,
+      payload: {
         name,
-        plantType,
         location,
         waterFrequency,
         fertilizingFrequency,
         light,
-        id
-    ) =>
-    async (dispatch) => {
-        dispatch({
-            type: actionTypes.PLANT_UPDATE_REQUEST,
-        });
-        try {
-            const userInfo = JSON.parse(localStorage.getItem('userInfo'));
-
-            const { data } = await axios.put(
-                `https://ntnu-plant-manager.herokuapp.com/plant/${id}`,
-                {
-                    name,
-                    plantType,
-                    location,
-                    waterFrequency,
-                    fertilizingFrequency,
-                    light,
-                },
-                {
-                    headers: { Authorization: `Bearer ${userInfo.token}` },
-                }
-            );
-            dispatch({
-                type: actionTypes.PLANT_UPDATE_SUCCESS,
-                payload: data,
-            });
-        } catch (error) {
-            dispatch({
-                type: actionTypes.PLANT_UPDATE_FAIL,
-                payload:
-                    error.response && error.response.data.message
-                        ? error.response.data.message
-                        : error.message,
-            });
-        }
-    };
-
-export const plantDelete = (id) => async (dispatch) => {
-    dispatch({
-        type: actionTypes.PLANT_DELETE_REQUEST,
-        payload: id,
+      },
     });
     try {
-        const userInfo = JSON.parse(localStorage.getItem('userInfo'));
+      const userInfo = JSON.parse(localStorage.getItem('userInfo'));
 
-        const { data } = await axios.delete(
-            `https://ntnu-plant-manager.herokuapp.com/plants/${id}`,
-            {
-                headers: { Authorization: `Bearer ${userInfo.token}` },
-            }
-        );
-        dispatch({
-            type: actionTypes.PLANT_DELETE_SUCCESS,
-            payload: data,
-        });
+      const { data } = await axios.post(
+        `${PLANT_API}/register`,
+        {
+          name,
+          location,
+          waterFrequency,
+          fertilizingFrequency,
+          light,
+        },
+        {
+          headers: { Authorization: `Bearer ${userInfo.token}` },
+        }
+      );
+      dispatch({
+        type: actionTypes.PLANT_CREATE_SUCCESS,
+        payload: data,
+      });
     } catch (error) {
-        dispatch({
-            type: actionTypes.PLANT_DELETE_FAIL,
-            payload:
-                error.response && error.response.data.message
-                    ? error.response.data.message
-                    : error.message,
-        });
+      dispatch({
+        type: actionTypes.PLANT_CREATE_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      });
     }
+  };
+
+export const updatePlant =
+  (
+    name,
+    plantType,
+    location,
+    waterFrequency,
+    fertilizingFrequency,
+    light,
+    id
+  ) =>
+  async (dispatch) => {
+    dispatch({
+      type: actionTypes.PLANT_UPDATE_REQUEST,
+    });
+    try {
+      const userInfo = JSON.parse(localStorage.getItem('userInfo'));
+
+      const { data } = await axios.put(
+        `${PLANT_API}/plant/${id}`,
+        {
+          name,
+          plantType,
+          location,
+          waterFrequency,
+          fertilizingFrequency,
+          light,
+        },
+        {
+          headers: { Authorization: `Bearer ${userInfo.token}` },
+        }
+      );
+      dispatch({
+        type: actionTypes.PLANT_UPDATE_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: actionTypes.PLANT_UPDATE_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      });
+    }
+  };
+
+export const plantDelete = (id) => async (dispatch) => {
+  dispatch({
+    type: actionTypes.PLANT_DELETE_REQUEST,
+    payload: id,
+  });
+  try {
+    const userInfo = JSON.parse(localStorage.getItem('userInfo'));
+
+    const { data } = await axios.delete(`${PLANT_API}/plants/${id}`, {
+      headers: { Authorization: `Bearer ${userInfo.token}` },
+    });
+    dispatch({
+      type: actionTypes.PLANT_DELETE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: actionTypes.PLANT_DELETE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
 };
 
 export const waterPlant = (id) => async (dispatch) => {
-    dispatch({
-        type: actionTypes.PLANT_WATER_REQUEST,
-    });
-    try {
-        const userInfo = JSON.parse(localStorage.getItem('userInfo'));
+  dispatch({
+    type: actionTypes.PLANT_WATER_REQUEST,
+  });
+  try {
+    const userInfo = JSON.parse(localStorage.getItem('userInfo'));
 
-        const { data } = await axios.put(
-            `https://ntnu-plant-manager.herokuapp.com/plant/water/${id}`,
-            {
-                id,
-            },
-            {
-                headers: { Authorization: `Bearer ${userInfo.token}` },
-            }
-        );
-        dispatch({
-            type: actionTypes.PLANT_WATER_SUCCESS,
-            payload: data,
-        });
-    } catch (error) {
-        dispatch({
-            type: actionTypes.PLANT_WATER_FAIL,
-            payload:
-                error.response && error.response.data.message
-                    ? error.response.data.message
-                    : error.message,
-        });
-    }
+    const { data } = await axios.put(
+      `${PLANT_API}/plant/water/${id}`,
+      {
+        id,
+      },
+      {
+        headers: { Authorization: `Bearer ${userInfo.token}` },
+      }
+    );
+    dispatch({
+      type: actionTypes.PLANT_WATER_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: actionTypes.PLANT_WATER_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
 };
 
 export const fertilizePlant = (id) => async (dispatch) => {
-    dispatch({
-        type: actionTypes.PLANT_FERTILIZE_REQUEST,
-    });
-    try {
-        const userInfo = JSON.parse(localStorage.getItem('userInfo'));
+  dispatch({
+    type: actionTypes.PLANT_FERTILIZE_REQUEST,
+  });
+  try {
+    const userInfo = JSON.parse(localStorage.getItem('userInfo'));
 
-        const { data } = await axios.put(
-            `https://ntnu-plant-manager.herokuapp.com/plant/fertilize/${id}`,
-            {
-                id,
-            },
-            {
-                headers: { Authorization: `Bearer ${userInfo.token}` },
-            }
-        );
-        dispatch({
-            type: actionTypes.PLANT_FERTILIZE_SUCCESS,
-            payload: data,
-        });
-    } catch (error) {
-        dispatch({
-            type: actionTypes.PLANT_FERTILIZE_FAIL,
-            payload:
-                error.response && error.response.data.message
-                    ? error.response.data.message
-                    : error.message,
-        });
-    }
+    const { data } = await axios.put(
+      `${PLANT_API}/plant/fertilize/${id}`,
+      {
+        id,
+      },
+      {
+        headers: { Authorization: `Bearer ${userInfo.token}` },
+      }
+    );
+    dispatch({
+      type: actionTypes.PLANT_FERTILIZE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: actionTypes.PLANT_FERTILIZE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
 };
--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -1,236 +1,230 @@
 import * as actionTypes from '../constants/userConstants';
 import axios from 'axios';
 
+require('dotenv').config();
+const { PLANT_API } = process.env;
+
 const userInfo = JSON.parse(localStorage.getItem('userInfo'));
 
 let token = undefined;
 
 if (userInfo) {
-    token = userInfo.token;
+  token = userInfo.token;
 }
 
 export const getUsers = () => async (dispatch) => {
-    try {
-        dispatch({ type: actionTypes.GET_USERS_REQUEST });
+  try {
+    dispatch({ type: actionTypes.GET_USERS_REQUEST });
 
-        const { data } = await axios.get(
-            'https://ntnu-plant-manager.herokuapp.com/users',
-            {
-                headers: { Authorization: `Bearer ${token}` },
-            }
-        );
+    const { data } = await axios.get(`${PLANT_API}/users`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
 
-        dispatch({
-            type: actionTypes.GET_USERS_SUCCESS,
-            payload: data,
-        });
-    } catch (error) {
-        dispatch({
-            type: actionTypes.GET_USERS_FAIL,
-            payload:
-                error.response && error.response.data.message
-                    ? error.response.data.message
-                    : error.message,
-        });
-    }
+    dispatch({
+      type: actionTypes.GET_USERS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: actionTypes.GET_USERS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
 };
 
 export const getUserDetails = (id) => async (dispatch, getState) => {
-    const {
-        userLogin: { userInfo },
-    } = getState();
-    dispatch({ type: actionTypes.GET_USER_DETAILS_REQUEST });
-    try {
-        const { data } = await axios.get(
-            `https://ntnu-plant-manager.herokuapp.com/users/${id}`,
-            {
-                headers: { Authorization: `Bearer ${token}` },
-            }
-        );
+  const {
+    userLogin: { userInfo },
+  } = getState();
+  dispatch({ type: actionTypes.GET_USER_DETAILS_REQUEST });
+  try {
+    const { data } = await axios.get(`${PLANT_API}/users/${id}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
 
-        dispatch({
-            type: actionTypes.GET_USER_DETAILS_SUCCESS,
-            payload: data,
-        });
-    } catch (error) {
-        dispatch({
-            type: actionTypes.GET_USER_DETAILS_FAIL,
-            payload:
-                error.response && error.response.data.message
-                    ? error.response.data.message
-                    : error.message,
-        });
-    }
+    dispatch({
+      type: actionTypes.GET_USER_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: actionTypes.GET_USER_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
 };
 
 export const removeUserDetails = () => (dispatch) => {
-    dispatch({
-        type: actionTypes.GET_USER_DETAILS_RESET,
-    });
+  dispatch({
+    type: actionTypes.GET_USER_DETAILS_RESET,
+  });
 };
 
 // Login
 export const login = (email, password) => async (dispatch) => {
-    dispatch({
-        type: actionTypes.USER_LOGIN_REQUEST,
-        payload: { email, password },
+  dispatch({
+    type: actionTypes.USER_LOGIN_REQUEST,
+    payload: { email, password },
+  });
+  try {
+    const { data } = await axios.post(`${PLANT_API}/login`, {
+      email,
+      password,
     });
-    try {
-        const { data } = await axios.post(
-            'https://ntnu-plant-manager.herokuapp.com/login',
-            { email, password }
-        );
-        dispatch({
-            type: actionTypes.USER_LOGIN_SUCCESS,
-            payload: data,
-        });
-        localStorage.setItem('userInfo', JSON.stringify(data));
-        token = data.token;
-    } catch (error) {
-        dispatch({
-            type: actionTypes.USER_LOGIN_FAIL,
-            payload:
-                error.response && error.response.data.message
-                    ? error.response.data.message
-                    : error.message,
-        });
-    }
+    dispatch({
+      type: actionTypes.USER_LOGIN_SUCCESS,
+      payload: data,
+    });
+    localStorage.setItem('userInfo', JSON.stringify(data));
+    token = data.token;
+  } catch (error) {
+    dispatch({
+      type: actionTypes.USER_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
 };
 
 export const logout = () => (dispatch) => {
-    localStorage.removeItem('userInfo');
-    localStorage.removeItem('notify');
-    localStorage.removeItem('notified');
-    dispatch({ type: actionTypes.USER_LOGIN_SIGNOUT });
+  localStorage.removeItem('userInfo');
+  localStorage.removeItem('notify');
+  localStorage.removeItem('notified');
+  dispatch({ type: actionTypes.USER_LOGIN_SIGNOUT });
 };
 
 export const register =
-    (firstName, lastName, email, password) => async (dispatch) => {
-        dispatch({
-            type: actionTypes.USER_REGISTER_REQUEST,
-            payload: { email, password },
-        });
-        try {
-            const { data } = await axios.post(
-                'https://ntnu-plant-manager.herokuapp.com/register',
-                {
-                    firstName,
-                    lastName,
-                    email,
-                    password,
-                },
-                {
-                    headers: { Authorization: `Bearer ${token}` },
-                }
-            );
-            dispatch({
-                type: actionTypes.USER_REGISTER_SUCCESS,
-                payload: data,
-            });
-            // to update state
-            // dispatch({
-            //     type: actionTypes.USER_LOGIN_SUCCESS,
-            //     payload: data,
-            // });
-            // localStorage.setItem('userInfo', JSON.stringify(data))
-        } catch (error) {
-            dispatch({
-                type: actionTypes.USER_REGISTER_FAIL,
-                payload:
-                    error.response && error.response.data.message
-                        ? error.response.data.message
-                        : error.message,
-            });
-        }
-    };
-
-export const updateUser =
-    (firstName, lastName, email, role, id) => async (dispatch) => {
-        dispatch({
-            type: actionTypes.USER_UPDATE_REQUEST,
-        });
-        try {
-            const { data } = await axios.put(
-                `https://ntnu-plant-manager.herokuapp.com/users/${id}`,
-                {
-                    firstName,
-                    lastName,
-                    email,
-                    role,
-                },
-                {
-                    headers: { Authorization: `Bearer ${token}` },
-                }
-            );
-            dispatch({
-                type: actionTypes.USER_UPDATE_SUCCESS,
-                payload: data,
-            });
-        } catch (error) {
-            dispatch({
-                type: actionTypes.USER_UPDATE_FAIL,
-                payload:
-                    error.response && error.response.data.message
-                        ? error.response.data.message
-                        : error.message,
-            });
-        }
-    };
-
-export const updateUserProfile =
-    (firstName, lastName, id) => async (dispatch) => {
-        dispatch({
-            type: actionTypes.USER_UPDATE_PROFILE_REQUEST,
-        });
-        try {
-            const { data } = await axios.put(
-                `https://ntnu-plant-manager.herokuapp.com/users/${id}`,
-                {
-                    firstName,
-                    lastName,
-                },
-                {
-                    headers: { Authorization: `Bearer ${token}` },
-                }
-            );
-            dispatch({
-                type: actionTypes.USER_UPDATE_PROFILE_SUCCESS,
-                payload: data,
-            });
-        } catch (error) {
-            dispatch({
-                type: actionTypes.USER_UPDATE_PROFILE_FAIL,
-                payload:
-                    error.response && error.response.data.message
-                        ? error.response.data.message
-                        : error.message,
-            });
-        }
-    };
-
-export const userDelete = (id) => async (dispatch) => {
+  (firstName, lastName, email, password) => async (dispatch) => {
     dispatch({
-        type: actionTypes.USER_DELETE_REQUEST,
-        payload: id,
+      type: actionTypes.USER_REGISTER_REQUEST,
+      payload: { email, password },
     });
     try {
-        const { data } = await axios.delete(
-            `https://ntnu-plant-manager.herokuapp.com/users/${id}`,
-            {
-                headers: { Authorization: `Bearer ${token}` },
-            }
-        );
-        dispatch({
-            type: actionTypes.USER_DELETE_SUCCESS,
-            payload: data,
-        });
+      const { data } = await axios.post(
+        `${PLANT_API}/register`,
+        {
+          firstName,
+          lastName,
+          email,
+          password,
+        },
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      dispatch({
+        type: actionTypes.USER_REGISTER_SUCCESS,
+        payload: data,
+      });
+      // to update state
+      // dispatch({
+      //     type: actionTypes.USER_LOGIN_SUCCESS,
+      //     payload: data,
+      // });
+      // localStorage.setItem('userInfo', JSON.stringify(data))
     } catch (error) {
-        dispatch({
-            type: actionTypes.USER_DELETE_FAIL,
-            payload:
-                error.response && error.response.data.message
-                    ? error.response.data.message
-                    : error.message,
-        });
+      dispatch({
+        type: actionTypes.USER_REGISTER_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      });
     }
+  };
+
+export const updateUser =
+  (firstName, lastName, email, role, id) => async (dispatch) => {
+    dispatch({
+      type: actionTypes.USER_UPDATE_REQUEST,
+    });
+    try {
+      const { data } = await axios.put(
+        `${PLANT_API}/users/${id}`,
+        {
+          firstName,
+          lastName,
+          email,
+          role,
+        },
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      dispatch({
+        type: actionTypes.USER_UPDATE_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: actionTypes.USER_UPDATE_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      });
+    }
+  };
+
+export const updateUserProfile =
+  (firstName, lastName, id) => async (dispatch) => {
+    dispatch({
+      type: actionTypes.USER_UPDATE_PROFILE_REQUEST,
+    });
+    try {
+      const { data } = await axios.put(
+        `${PLANT_API}/users/${id}`,
+        {
+          firstName,
+          lastName,
+        },
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      dispatch({
+        type: actionTypes.USER_UPDATE_PROFILE_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: actionTypes.USER_UPDATE_PROFILE_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      });
+    }
+  };
+
+export const userDelete = (id) => async (dispatch) => {
+  dispatch({
+    type: actionTypes.USER_DELETE_REQUEST,
+    payload: id,
+  });
+  try {
+    const { data } = await axios.delete(`${PLANT_API}/users/${id}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    dispatch({
+      type: actionTypes.USER_DELETE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: actionTypes.USER_DELETE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
 };
